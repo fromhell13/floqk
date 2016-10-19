0fad6762fb2f37c26ac015ffc582d5a5a068c427
fromhell <zalizan13@gmail.com>
2016-10-19 16:51:37 +0800

    First commit

---
+++ www/lib/firebase/firebase.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.4.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCudwp9KZRx8l9afNRhwY4rViiuXeuZf0k",
+    authDomain: "floqk-616b2.firebaseapp.com",
+    databaseURL: "https://floqk-616b2.firebaseio.com",
+    storageBucket: "floqk-616b2.appspot.com",
+    messagingSenderId: "397401274790"
+  };
+  firebase.initializeApp(config);
+</script>
